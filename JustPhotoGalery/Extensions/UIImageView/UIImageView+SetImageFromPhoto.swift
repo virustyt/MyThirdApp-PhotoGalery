284f0730fc00a33c0b195f646415858661321146
Vladimir Oleinikov <dragon2020@inbox.ru>
2022-02-13 00:36:55 +0300

    feat: change api of ImageNetClientProtocol, add tag to SetImageFromPhoto method

--- JustPhotoGalery/Extensions/UIImageView/UIImageView+SetImageFromPhoto.swift
+++ JustPhotoGalery/Extensions/UIImageView/UIImageView+SetImageFromPhoto.swift
@@ -16,6 +16,23 @@ extension UIImageView {
             self.image = UIImage(named: "placeholder")
             return
         }
-        ImageClient.shared.setImage(on: self, from: photosUrl, with: UIImage(named: "placeholder"))
+        let hash = photosName.hashValue
+        tag = hash
+
+        image = UIImage(named: "placeholder")
+        ImageClient.shared.getImage(from: photosUrl, complition: {
+            [weak self] image, error in
+
+            guard self?.tag == hash
+            else { return }
+
+            if let recievedImage = image {
+                self?.contentMode = .scaleAspectFill
+                self?.image = recievedImage
+            } else if let _ = error {
+                self?.contentMode = .scaleAspectFit
+                self?.image = UIImage(named: "imageNotFound2")
+            }
+        })
     }
 }
--- JustPhotoGalery/Network/ImageNetClient.swift
+++ JustPhotoGalery/Network/ImageNetClient.swift
@@ -10,42 +10,59 @@ import UIKit
 protocol ImageNetClientProtocol{
     func downloadImages(imageURL: URL ,
                         complition: @escaping (UIImage?, Error?) -> ()) -> URLSessionDataTask?
-    func setImage(on imageView: UIImageView,
-                  from imageURL: URL,
-                  with placeholder: UIImage?,
+    func getImage(from imageURL: URL,
                   complition: ((UIImage?, Error?)-> ())?)
 }
 
 class ImageClient {
-    var cashedImages: [URL: UIImage]
     var cashedDataTasks: [URL: URLSessionDataTask]
 
     var urlSession: URLSession
     var responseQueue: DispatchQueue?
+    private var cache: URLCache? {
+        urlSession.configuration.urlCache
+    }
 
-    static let shared = ImageClient(urlSession: URLSession.shared, responseQueue: .main)
+    private static var sharedInstanceCache: URLCache = {
+        let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        let diskCacheURL = cachesURL.appendingPathComponent("DownloadCache")
+        let cache = URLCache(memoryCapacity: 200_000_000, diskCapacity: 600_000_000, directory: diskCacheURL)
+        return cache
+    }()
+
+    static let shared: ImageClient = {
+        let configuration = URLSessionConfiguration.default
+        configuration.requestCachePolicy = .returnCacheDataElseLoad
+        configuration.urlCache = sharedInstanceCache
+        let session = URLSession(configuration: configuration)
+        let imageClient = ImageClient(urlSession: session, responseQueue: .main)
+        return imageClient
+    }()
 
     // MARK: - inits
     init(urlSession: URLSession, responseQueue: DispatchQueue?){
         self.urlSession = urlSession
         self.responseQueue = responseQueue
-
-        self.cashedImages = [:]
         self.cashedDataTasks = [:]
     }
 }
 
 extension ImageClient: ImageNetClientProtocol{
+
     func downloadImages(imageURL: URL, complition: @escaping (UIImage?, Error?) -> ()) -> URLSessionDataTask? {
-        if let cashedImage = self.cashedImages[imageURL] {
-            dispatchResults(model: cashedImage, complitionHandler: complition)
+        let imageRequest = URLRequest(url: imageURL)
+
+        if let cashedResponse = cache?.cachedResponse(for: imageRequest) {
+            dispatchResults(model: UIImage(data: cashedResponse.data), complitionHandler: complition)
             return nil
         }
+
         let dataTask = self.urlSession.dataTask(with: imageURL) { [weak self] data, response, error in
             guard let self = self else { return }
-            if let recievedData = data,
+            if let recievedResponse = response,
+               let recievedData = data,
                let recievedImage = UIImage(data: recievedData) {
-                self.cashedImages[imageURL] = recievedImage
+                self.cache?.storeCachedResponse(CachedURLResponse(response: recievedResponse, data: recievedData), for: imageRequest)
                 self.dispatchResults(model: recievedImage,complitionHandler: complition)
             }
             else{
@@ -56,21 +73,21 @@ extension ImageClient: ImageNetClientProtocol{
         return dataTask
     }
 
-    func setImage(on imageView: UIImageView, from imageURL: URL, with placeholder: UIImage?, complition: ((UIImage?, Error?)-> ())? = nil) {
+    func getImage(from imageURL: URL, complition: ((UIImage?, Error?)-> ())? = nil) {
         cashedDataTasks[imageURL]?.cancel()
-        imageView.image = placeholder
         cashedDataTasks[imageURL] = downloadImages(imageURL: imageURL,
                                                     complition: {[weak self] image, error in
                                                         guard let self = self else {return}
+
                                                         self.cashedDataTasks[imageURL] = nil
-                                                        if let notNilImage = image {imageView.image = notNilImage}
+
                                                         guard complition != nil else { return }
                                                         complition!(image, error)
                                                     })
         }
 
     func dispatchResults<Type>(model: Type? = nil, error: Error? = nil, complitionHandler: @escaping (Type?,Error?) -> ()){
-        guard let responseQueue = self.responseQueue
+        guard let responseQueue = responseQueue
         else {
             complitionHandler(model,error)
             return
