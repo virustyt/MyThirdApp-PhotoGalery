766408265a234faa44e9a3aaf6e9d063be1fd9f6
Vladimir Oleinikov <dragon2020@inbox.ru>
2022-02-13 12:07:35 +0300

    refactor: remove comments from setDoubleShadows method

--- JustPhotoGalery/Extensions/CALayer/CALayer+setDoubleShadow.swift
+++ JustPhotoGalery/Extensions/CALayer/CALayer+setDoubleShadow.swift
@@ -14,7 +14,6 @@ extension CALayer {
         layer0.frame = bounds
         layer0.backgroundColor = backgroundColor
         layer0.shadowColor = UIColor(red: 0.68, green: 0.68, blue: 0.75, alpha: 0.4).cgColor
-//        layer0.shadowOffset = CGSize(width: 1.5, height: 1.5)
         layer0.shadowOffset = CGSize(width: 3.5, height: 3.5)
         layer0.shadowRadius = 3
         layer0.shadowOpacity = 1
@@ -25,7 +24,6 @@ extension CALayer {
         layer1.frame = bounds
         layer1.backgroundColor = backgroundColor
         layer1.shadowColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-//        layer1.shadowOffset = CGSize(width: -1, height: -1)
         layer1.shadowOffset = CGSize(width: -4, height: -4)
         layer1.shadowRadius = 3
         layer1.shadowOpacity = 1
